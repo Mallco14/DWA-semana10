9ef8238b65e758d2f114fa20385283b7c03f628c
JhonM <jhon@example.com>
2022-11-08 11:16:04 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+import pictures from './assets/pictures';
+
+function App() {
+  const [number, setNumber] = useState(0);
+  
+  return (  
+    <div className="App">
+      <div className='Header'><Header /></div>   
+      
+      <div className='Medio'>
+        <div>
+          <img className='hood' src={pictures.hood} />
+          <img className='imagen' src={pictures.imagen}/>
+        </div>
+
+        <h1>Este es mi primer componente</h1>
+        <h2>El valor de number es: {number}</h2>
+        <button type="button" className="btn btn-success btn-lg" onClick={() => setNumber(number + 1)}>Incrementar</button> 
+        <button type="button" className="btn btn-danger btn-lg" onClick={() => setNumber(number - 1)}>Decrementar</button>
+      </div>                
+      <div className='Footer'>
+        <Footer />
+      </div> 
+    </div>
+  );
+ }
+  
+ export default App;
---
+++ src/assets/pictures.js
@@ -0,0 +1,6 @@
+import hood from "./img/hood.jpg"
+import imagen from "./img/imagen.png"
+export default {
+    "hood": hood,
+    "imagen": imagen
+}
